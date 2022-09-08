5cbc0c97a939c1e3282952c49a9c066cc652c818
Igor Bitencourt <bitencourt.igor@hotmail.com>
2022-09-08 19:43:13 -0300

    add array megasena

--- src/App.js
+++ src/App.js
@@ -2,10 +2,15 @@ import React from "react";
 import Button from "./Components/Button";
 
 function App() {
-  const jogo = [];
+  const [jogo, setJogo] = React.useState([]);
 
   function jogoMegasena() {
-    const jogar = Math.floor(Math.random() * 59 + 1);
+    let megasena = [];
+
+    while (megasena.length < 6) {
+      megasena.push(Math.floor(Math.random() * 59 + 1));
+    }
+    setJogo([...megasena]);
   }
 
   return (
