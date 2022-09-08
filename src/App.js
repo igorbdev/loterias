4f3c1e5c71e6d52b7abfe1b9e54a1c35ff3d5a88
Igor Bitencourt <bitencourt.igor@hotmail.com>
2022-09-07 22:26:01 -0300

    megasena

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import React from "react";
+import Button from "./Components/Button";
+
 function App() {
-  return <div>TESTE</div>;
+  const jogo = [];
+
+  function jogoMegasena() {
+    const jogar = Math.floor(Math.random() * 59 + 1);
+  }
+
+  return (
+    <div>
+      <Button onClick={jogoMegasena}>MegaSena</Button>
+      {jogo.map((numero) => (
+        <p key={numero.indexOf}>{numero}</p>
+      ))}
+    </div>
+  );
 }
 
 export default App;
